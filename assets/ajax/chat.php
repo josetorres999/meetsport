43725d22c1d8c7d82c552c320ff998cf9136e5fb
josetorres999 <jatorrespalma@gmail.com>
2022-12-15 04:30:14 +0100

    Chat

--- assets/ajax/chat.php
+++ assets/ajax/chat.php
@@ -1,12 +1,32 @@
 <?php
+    include_once("../conexion/conexion.php");
 
+
+    //Escribir mensaje
     $result = array();
-    $mensaje = $_POST['mensaje'];
-    $id_usuario = $_POST['id_usuario'];
-    $id_evento = $_POST['id_evento'];
+    $mensaje = isset($_POST['mensaje']) ? $_POST['mensaje'] : null;
+    $nombre =isset($_POST['nombre']) ? $_POST['nombre'] : null;
+    $id_evento = isset($_POST['id_evento']) ? $_POST['id_evento'] : null;
 
-    if(!empty($mensaje) && !empty($nombre)){
-        $sql = "INSERT INTO mensajes ('id_evento','id_usuario','mensaje') VALUES($id_evento,$id_usuario,$mensaje)";
+    if(!empty($mensaje) && !empty($nombre) && !empty($id_evento)){
+        $sql = "INSERT INTO mensajes (id_evento, nombre,mensaje) VALUES($id_evento,'$nombre','$mensaje')";
+        $result['send_status'] = $conn->query($sql);
     }
+
+    //Mostrar mensajes
+    $id_evento = isset($_GET['id']) ? $_GET['id'] : 0;
+    $start = isset($_GET['start']) ? $_GET['start'] : 0;
+
+    $todos = $conn->query("SELECT * FROM mensajes WHERE id_evento=".$id_evento." AND id > ".$start);
+    while($row = $todos->fetch_assoc()){
+        $result['todos'][] = $row;
+    }
+
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    
+
+    echo json_encode($result);
 
 ?>
--- assets/views/chat.php
+++ assets/views/chat.php
@@ -1,14 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+        session_start();
+        include_once("../conexion/conexion.php");
+
+        $consulta = "SELECT nom_us FROM usuarios WHERE id=".$_SESSION['id'];
+        $resnombre = $conn->query($consulta);
+        $nombre = $resnombre->fetch_assoc()['nom_us'];
+        
+
+
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="width=device-width, initianl-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
     <script>
-        var from = null, start = 0;
+        var from = null, start = 0, url="../ajax/chat.php";
         $(document).ready(function(){
-            from = "Nombre";
+
+            cargarMensajes();
+
+            $('form').submit(function(e){
+                e.preventDefault();
+                parametros = {
+                    "mensaje" : $('#mensaje').val(),
+                    "nombre" : '<?=$nombre?>',
+                    "id_evento" : <?=$_GET['id']?>
+                }
+                $.post(url,parametros);
+                $('#mensaje').val("");
+                return false;
+            })
+
+            function cargarMensajes(){
+                $.get(url+"?id="+<?=$_GET['id']?>+"&start="+start, function(result){
+                    if(result.todos){
+                        result.todos.forEach(item =>{
+                            start = item.id;
+                            $('#mensajes').append(renderMensaje(item));
+                        })
+                        $('#mensajes').animate({scrollTop: $('#mensajes')[0].scrollHeight});
+                    }
+                    cargarMensajes();
+                })
+            }
+
+
+
+            function renderMensaje(item){
+                let time = new Date(item.fecha)
+                time = `${time.getHours()}:${time.getMinutes()}`
+                return '<div class = "msg"><p>'+item.nombre+'</p>'+item.mensaje+'<span>'+time+'</span></div>';
+            }
+            
 
         })
     </script>
@@ -38,6 +84,21 @@
         }
         #mensaje{
             flex:2;
+        }
+        .msg{
+            background-color: #dcf8dc;
+            padding: 5px 10px;
+            border-radius: 5px;
+            margin-bottom: 8px;
+            width: fit-content;
+        }
+        .msg p{
+            margin: 0;
+            font-weight: bold;
+        }
+        .msg span{
+            font-size: 0.7rem;
+            margin-left: 15px;
         }
     </style>
 </head>
